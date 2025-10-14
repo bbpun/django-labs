52e3f80d5f96d7f39d0e70a00a0b268e3709c421
bbpun <bbpun@ssnbuilders.com>
2025-10-14 12:51:00 +0545

    update: setting up, styling etc.

--- 02BlogApp/config/settings.py
+++ 02BlogApp/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATICFILES_DIRS = [BASE_DIR / 'blog/static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login redirect URL
+LOGIN_REDIRECT_URL = 'home'
+
+# Logout redirect URL
+LOGOUT_REDIRECT_URL = 'home'
