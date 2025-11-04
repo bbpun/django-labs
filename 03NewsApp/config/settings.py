67190adc4c619f07d4f7a667c5178a4cdeb63eb1
bbpun <bbpun@ssnbuilders.com>
2025-11-04 10:45:13 +0545

    update: change to default setting of database

--- 03NewsApp/config/settings.py
+++ 03NewsApp/config/settings.py
@@ -87,7 +87,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL") 
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
